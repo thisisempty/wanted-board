537c4359a511f5e1543c29ad6c96b23c0a13eb94
thisisempty <thisisemptyyy@gmail.com>
2021-10-24 16:02:26 +0900

    [MODIFY] settings.py

--- wanted/settings.py
+++ wanted/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from my_settings import SECRET_KEY, ALGORITHM
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dhd((!$s6*!+wh#bp60$%)vkkgv6+@q@98(vsy3xs+4zyk4us%'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -154,4 +156,4 @@ CORS_ALLOW_HEADERS = (
     'x-requested-with',
 )
 
-ALGORITHM = 'HS256'
+ALGORITHM = ALGORITHM
